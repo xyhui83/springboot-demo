721e8dda2658f265706b8a172d2f34450019994d
xyhui83 <xyhui83@qq.com>
2019-04-14 11:20:02 +0800

    init

---
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -0,0 +1,20 @@
+package com.example.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Demo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo2Application.class, args);
+	}
+
+	@RequestMapping("/index")
+	public String hello() {
+		return "hello docker";
+	}
+}
